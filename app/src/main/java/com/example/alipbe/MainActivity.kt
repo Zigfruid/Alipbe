dd6aba03e15911bbadee247054981a5efa96554e
Zigfruid <Zigfruid@gmail.com>
2020-09-11 18:53:59 +0500

    letters was not done

--- app/src/main/java/com/example/alipbe/MainActivity.kt
+++ app/src/main/java/com/example/alipbe/MainActivity.kt
@@ -9,9 +9,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnStart.setOnClickListener {
-
-        }
-
     }
 }
