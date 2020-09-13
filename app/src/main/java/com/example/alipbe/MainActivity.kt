01afa191b577c9beeac6c6987502c3ded9a38a58
MambetniyazovAmir <mr.amirniyaz@mail.ru>
2020-09-13 14:49:31 +0500

    navigation implemented

--- app/src/main/java/com/example/alipbe/MainActivity.kt
+++ app/src/main/java/com/example/alipbe/MainActivity.kt
@@ -2,12 +2,12 @@ package com.example.alipbe
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
---
+++ app/src/main/java/com/example/alipbe/letters/LettersFragment.kt
@@ -0,0 +1,8 @@
+package com.example.alipbe.letters
+
+import androidx.fragment.app.Fragment
+import com.example.alipbe.R
+
+class LettersFragment: Fragment(R.layout.fragment_letters) {
+
+}
