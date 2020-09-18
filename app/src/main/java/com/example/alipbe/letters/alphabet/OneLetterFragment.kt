cca160319094db0fd641eefcc93c025c6ab94c28
MambetniyazovAmir <mr.amirniyaz@mail.ru>
2020-09-18 13:46:33 +0500

    soundBtn done

--- app/src/main/java/com/example/alipbe/letters/alphabet/OneLetterFragment.kt
+++ app/src/main/java/com/example/alipbe/letters/alphabet/OneLetterFragment.kt
@@ -1,24 +1,42 @@
 package com.example.alipbe.letters.alphabet
 
+import android.media.AudioAttributes
+import android.media.SoundPool
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.alipbe.R
 import kotlinx.android.synthetic.main.fragment_one_letter.*
 
 class OneLetterFragment : Fragment(R.layout.fragment_one_letter) {
 
+    private lateinit var soundPool: SoundPool
     private val safeArgs: OneLetterFragmentArgs by navArgs()
     private var currentId: Int = 1
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val navController = Navigation.findNavController(view)
+        val audioAttributes = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .build()
+        soundPool = SoundPool.Builder()
+            .setMaxStreams(1)
+            .setAudioAttributes(audioAttributes)
+            .build()
         currentId = safeArgs.letterId
         setImg(currentId)
+        var sound = soundPool.load(
+            context,
+            resources.getIdentifier("letter$currentId", "raw", requireActivity().packageName),
+            1
+        )
+        soundPool.setOnLoadCompleteListener { soundPool, sampleId, status ->
+            soundPool.play(sampleId, 1F, 1F, 0, 1, 1F)
+        }
         btnNext.setOnClickListener {
             currentId += 1
             if (currentId < 35) {
@@ -27,6 +45,11 @@ class OneLetterFragment : Fragment(R.layout.fragment_one_letter) {
                 currentId = 1
                 setImg(currentId)
             }
+            sound = soundPool.load(
+                context,
+                resources.getIdentifier("letter$currentId", "raw", requireActivity().packageName),
+                1
+            )
         }
         btnPrev.setOnClickListener {
             if (currentId > 1) {
@@ -36,9 +59,17 @@ class OneLetterFragment : Fragment(R.layout.fragment_one_letter) {
                 currentId = 34
                 setImg(currentId)
             }
+            sound = soundPool.load(
+                context,
+                resources.getIdentifier("letter$currentId", "raw", requireActivity().packageName),
+                1
+            )
         }
         homeBtn.setOnClickListener {
             navController.popBackStack()
+        }
+        soundBtn.setOnClickListener {
+            soundPool.play(sound, 1F, 1F, 0, 0, 1F)
         }
     }
 
@@ -50,5 +81,10 @@ class OneLetterFragment : Fragment(R.layout.fragment_one_letter) {
                 requireActivity().packageName
             )
         )
+    }
+
+    override fun onDestroy() {
+        soundPool.release()
+        super.onDestroy()
     }
 }
